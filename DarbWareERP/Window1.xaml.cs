78191734459cd5d69c429d455f9d1e4d09c881e8
張騰升 <momo16542@gmail.com>
2015-11-16 12:23:40 +0800

    登入視窗設計

--- DarbWareERP/Window1.xaml.cs
+++ DarbWareERP/Window1.xaml.cs
@@ -10,29 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace DarbWareERP
 {
     /// <summary>
-    /// MainWindow.xaml 的互動邏輯
+    /// Window1.xaml 的互動邏輯
     /// </summary>
-    public partial class 登入視窗 : Window
+    public partial class Window1 : Window
     {
-        public 登入視窗()
+        public Window1()
         {
             InitializeComponent();
-        }
-
-        private void btn離開_Click(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
-        }
-
-        private void btn登入_Click(object sender, RoutedEventArgs e)
-        {
-           
         }
     }
 }
