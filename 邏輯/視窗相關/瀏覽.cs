3f10b126169256328ffc2a862b91af115aceb956
張騰升 <momo16542@gmail.com>
2016-07-03 17:03:38 +0800

    00

---
+++ 邏輯/視窗相關/瀏覽.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 數據庫連線;
+using System.Data;
+
+namespace 邏輯.視窗相關
+{
+    public class 瀏覽
+    {
+        public DataSet 瀏覽查詢(string dataevent, string 查詢條件)
+        {
+            DataSet ds = Log.Log_Sys_Exec(dataevent + "Browse", "DARB_BROWSE", ref dataevent, 查詢條件, "2", "0");
+            return ds;
+        }
+    }
+}
